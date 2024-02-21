edd441123acb5db61d5a27499a3645b021b8485b
David VanScott <david.vanscott@gmail.com>
2024-02-20 21:07:28 -0500

    Adding incorrect compilation example

---
+++ resources/views/incorrect.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Incorrect</title>
+</head>
+<body class="font-sans antialiased">
+    @php
+        $callout = 'Laravel';
+        $heading = 'Hello world!';
+        $description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, numquam provident. Iusto atque laboriosam optio repellendus? Quaerat aliquid possimus magni est? Architecto, error quidem? Enim cupiditate mollitia laboriosam dignissimos perspiciatis.';
+    @endphp
+
+    <div class="@container">
+        <div class="mx-auto max-w-7xl py-16">
+            <div class="@xs:mt-24 @md:mt-32 @xl:mt-16">
+                {{ $callout }}
+            </div>
+
+            <h1
+                @class([
+                    'font-bold tracking-tight text-gray-900 @xs:text-4xl @md:text-6xl',
+                    'mt-10' => filled($callout),
+                    '@xs:mt-24 @md:mt-32 @xl:mt-16' => blank($callout)
+                ])
+            >
+                {{ $heading }}
+            </h1>
+
+            <div
+                @class([
+                    'text-lg/8 text-gray-600',
+                    'mt-6' => filled($heading),
+                    'mt-10' => filled($callout) && blank($heading),
+                    '@xs:mt-24 @md:mt-32 @xl:mt-16' => blank($callout) && blank($heading),
+                ])
+            >
+                {{ $description }}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 
 Route::view('failure', 'failure');
 Route::view('success', 'success');
+Route::view('incorrect', 'incorrect');
